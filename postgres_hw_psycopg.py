f616389f38673e5c375c8d4404639892c7bb795e
eugiv <eugene.ivanov@ymail.com>
2023-12-06 17:54:07 +0300

    connection issue

--- postgres_hw_psycopg.py
+++ postgres_hw_psycopg.py
@@ -13,8 +13,6 @@ class DBConnector:
         self.ssh_port = ssh_port
         self.database_user = database_user
         self.database = database
-        self.conn = None
-        self.cursor = None
 
     def connection(self):
         with open(self.sens_file) as f:
@@ -37,25 +35,39 @@ class DBConnector:
                 database=self.database,
                 password=postgres_password)
 
-            self.conn = conn
-            self.cursor = conn.cursor()
-
-    def get_cursor(self):
-        if not self.cursor:
-            self.connection()
-        return self.cursor
+        return conn
 
 class CustomerDB:
     def __init__(self, db_connector: DBConnector):
-        self.conn = db_connector
+        self.connector = db_connector
     def create_table(self):
-        with self.conn.get_cursor() as cur:
-            print(cur)
-            cur.execute("CREATE TABLE test_table(id SERIAL PRIMARY KEY, name VARCHAR(40));")
-            cur.commit()
+        print()
+        with self.connector.connection().cursor() as cur:
+            print(self.connector.connection())
+            cur.execute("CREATE TABLE test_table12 (id SERIAL PRIMARY KEY, name VARCHAR(40));")
+            self.connector.connection().commit()
 
 
 connection1 = DBConnector('sens.txt', 'localhost', 5432, 'ubuntu',
                           22, 'postgres', 'netology_psycopg2')
 db_run = CustomerDB(connection1)
 db_run.create_table()
+
+# with (SSHTunnelForwarder(
+#         ('xxxxxxxxxxxxxx.compute-1.amazonaws.com', 22),
+#         ssh_host_key=None,
+#         ssh_username='ubuntu',
+#         ssh_password=None,
+#         ssh_pkey='/Users/eugene_ivanov/AWS/EC2/linux_server/eug_linux_server_key.pem',
+#         remote_bind_address=('localhost', 5432))
+#         as ec2_tunnel):
+#     conn = pg.connect(
+#         host='localhost',
+#         port=ec2_tunnel.local_bind_port,
+#         user='postgres',
+#         database='netology_psycopg2',
+#         password='some password')
+#
+#     with conn.cursor() as cur:
+#         cur.execute('CREATE TABLE test_table1 (id SERIAL PRIMARY KEY, name VARCHAR(40));')
+#         conn.commit()
